e725c3b75a58d53fc064aa6d8f3dfb1f6f6df839
richiehydra <richiehydra22@gmail.com>
2023-07-19 14:25:01 +0530

    done

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
-const express=require("express");
-const mongoose=require("express");
+import express from "express"
+import mongoose from "mongoose"
 const app = express();
 
 const PORT = 5000;
 
 
 
-mongoose.connnect().then(() => console.log("SuccesFully")).catch((err) => console.log("Unsuccesfull connection"))
+mongoose.connect().then(() => console.log("SuccesFully")).catch((err) => console.log("Unsuccesfull connection"))
 
 
 app.listen(PORT, () => {
