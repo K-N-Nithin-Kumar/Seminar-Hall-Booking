8b31a72977aa2f4e0064bd1cd6c3567b61641bf6
Prashanth <prashanthranjith789@gmail.com>
2023-07-20 18:38:59 +0530

    camelCae

--- backend/config/dbConnection.js
+++ backend/config/dbConnection.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
-//Today Nightout
+//Today Nightout ok
 const connectDB = async ()=>{
     try{
-        const connect = await mongoose.connect("mongodb+srv://admin:<password>@ineuron.8h2plss.mongodb.net/seminar-hall?retryWrites=true&w=majority")
+        const connect = await mongoose.connect("mongodb+srv://poojarii018:70NuGdSg0yRhVxOM@cluster0.qzbygiw.mongodb.net/")
         console.log("Database connected ",connect.connection.host , connect.connection.name);
     }catch(err){
         console.log(err);
